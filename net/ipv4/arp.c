cbe44d98046bba12bfa4343a9c04af0fc95db435
jollaman999 <admin@jollaman999.com>
2016-05-31 16:36:39 +0900

    arp: Add print_arp_info option

--- net/ipv4/arp.c
+++ net/ipv4/arp.c
@@ -125,6 +125,7 @@
 #define ARP_PROJECT_VERSION "0.1"
 
 static bool arp_project_enable = true;
+static bool print_arp_info = false;
 
 unsigned long init_time;
 
@@ -321,7 +322,7 @@ static void arp_send_dst(int type, int ptype, __be32 dest_ip,
 		return;
 
 	/* arp_project - Print arp_ptr infos */
-	if (arp_project_enable)
+	if (arp_project_enable && print_arp_info)
 		arp_print_info(dev, arp_hdr(skb), 1);
 
 	skb_dst_set(skb, dst_clone(dst));
@@ -890,7 +891,7 @@ static int arp_process(struct net *net, struct sock *sk, struct sk_buff *skb)
 		goto out;
 
 	/* arp_project - Print arp_ptr infos */
-	if (arp_project_enable)
+	if (arp_project_enable && print_arp_info)
 		arp_print_info(dev, arp, 0);
 
 /*
@@ -1732,6 +1733,46 @@ static ssize_t arp_project_enable_dump(struct device *dev,
 static DEVICE_ATTR(arp_project_enable, (S_IWUSR|S_IRUGO),
 	arp_project_enable_show, arp_project_enable_dump);
 
+static ssize_t print_arp_info_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	size_t count = 0;
+
+	count += sprintf(buf, "%d\n", print_arp_info);
+
+	return count;
+}
+
+static ssize_t print_arp_info_dump(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	int val = 0;
+
+	if (print_arp_info)
+		val = 1;
+
+	if ((buf[0] == '0' || buf[0] == '1') && buf[1] == '\n') {
+		if (val != buf[0] - '0')
+			val = buf[0] - '0';
+		else
+			return count;
+	} else
+		return -EINVAL;
+
+	if (val) {
+		print_arp_info = true;
+		printk(ARP_PROJECT"%s: Enabled\n", __func__);
+	} else {
+		print_arp_info = false;
+		printk(ARP_PROJECT"%s: Disabled\n", __func__);
+	}
+
+	return count;
+}
+
+static DEVICE_ATTR(print_arp_info, (S_IWUSR|S_IRUGO),
+	print_arp_info_show, print_arp_info_dump);
+
 struct kobject *arp_project_kobj;
 
 static void __init arp_sys_init(void)
@@ -1751,5 +1792,10 @@ static void __init arp_sys_init(void)
 	rc = sysfs_create_file(arp_project_kobj, &dev_attr_arp_project_enable.attr);
 	if (rc) {
 		pr_warn("%s: sysfs_create_file failed for arp_project_enable\n", __func__);
+	}
+
+	rc = sysfs_create_file(arp_project_kobj, &dev_attr_print_arp_info.attr);
+	if (rc) {
+		pr_warn("%s: sysfs_create_file failed for print_arp_info\n", __func__);
 	}
 }
